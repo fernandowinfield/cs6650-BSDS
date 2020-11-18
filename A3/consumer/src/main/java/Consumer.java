29070d41e22a9186812c4d439be1b91a61f69155
Fernando Winfield Murillo <winfieldmurillo.f@husky.neu.edu>
2020-11-18 11:44:08 -0800

    Change consumers threads to 20 and switch back to free-tier RDS instance

--- A3/consumer/src/main/java/Consumer.java
+++ A3/consumer/src/main/java/Consumer.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 
 public class Consumer {
   private static final String QUEUE_NAME = "postingQueue";
-  private static final int CONSUMER_THREADS_NUM = 200;
+  private static final int CONSUMER_THREADS_NUM = 20;
 
   public static void main(String[] argv) throws Exception {
     ConnectionFactory factory = new ConnectionFactory();
--- A3/server/src/main/java/LiftRideDao.java
+++ A3/server/src/main/java/LiftRideDao.java
@@ -93,17 +93,28 @@ public class LiftRideDao {
   /*
    * For GET /skiers/{skierID}/vertical
    */
-  // TODO: maybe can update this if we decide to go with "cache" table strategy (only have to SELECT from "verticals cache" table)
   public int getVertical2(String skierID, String resortID) {
     Connection conn = null;
     PreparedStatement preparedStatement = null;
-    String selectQueryStatement = "SELECT SUM(vertical) AS totalVertical FROM IkkyoneSkiing.LiftRides WHERE skierID=? AND resortID=?;";
+
+    // The following query statement is optimized specifically for the experiment set up where there
+    // will only be 1 resortID. If there were to be multiple resorts involved, we could convert the
+    // primary key for the Verticals table to be a composite key (using the skierID and resortID columns),
+    // and therefore the query statement (below) would just have to add one more check (i.e. `AND resortID=?`).
+    String selectQueryStatement = "SELECT vertical AS totalVertical from IkkyoneSkiing.Verticals WHERE skierID=?;";
     ResultSet results = null;
     try {
       conn = dataSource.getConnection();
       preparedStatement = conn.prepareStatement(selectQueryStatement);
       preparedStatement.setString(1, skierID);
-      preparedStatement.setString(2, resortID);
+
+//    String selectQueryStatement = "SELECT SUM(vertical) AS totalVertical FROM IkkyoneSkiing.LiftRides WHERE skierID=? AND resortID=?;";
+//    ResultSet results = null;
+//    try {
+//      conn = dataSource.getConnection();
+//      preparedStatement = conn.prepareStatement(selectQueryStatement);
+//      preparedStatement.setString(1, skierID);
+//      preparedStatement.setString(2, resortID);
 
       // execute SELECT SQL statement
       results = preparedStatement.executeQuery();
